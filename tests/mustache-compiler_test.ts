f2d41a1894bb24e32b8e4859ba957d58fe7360f8
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2022-12-14 16:56:27 +0000

    test: Mustache compiler

---
+++ tests/mustache-compiler_test.ts
@@ -0,0 +1,12 @@
+import { assertEquals } from './_dependencies.ts'
+import { compileMustaches } from '../components/_mustache-compiler.ts'
+
+Deno.test('Mustache compiler', () => {
+	assertEquals(compileMustaches('{"Hello world"}'), '${"Hello world"}')
+	assertEquals(compileMustaches('{123}{456}'), '${123}${456}')
+
+	assertEquals(
+		compileMustaches('{import.meta}👨‍👩‍👧‍👦{<br/>}{1}{2}{3}xyz{abc}'),
+		'${import.meta}👨‍👩‍👧‍👦${<br/>}${1}${2}${3}xyz${abc}',
+	)
+})
