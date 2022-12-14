60cc262418e6d5646f4e78f4431ae903c54d4594
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2022-12-14 16:53:19 +0000

    fix(compiler): Use same version for types as SWC

--- components/_dependencies.ts
+++ components/_dependencies.ts
@@ -1,3 +1,3 @@
-export * as SWCTypes from 'https://esm.sh/@swc/core@1.3.23/types.d.ts?bundle&dev&pin=v99'
+export * as SWCTypes from 'https://esm.sh/@swc/core@1.2.212/types.d.ts?bundle&dev&pin=v99'
 
 export * as SWC from 'https://deno.land/x/swc@0.2.1/mod.ts'
