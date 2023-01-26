a90c4149ce0e70bd6b66858a0cadd86101a4fafc
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-01-26 17:50:30 +0000

    fix(components): Many things

--- components/compiler.ts
+++ components/compiler.ts
@@ -1,18 +1,19 @@
-import { parse, print } from './_dependencies.ts'
+import { parse, print } from './_estree.ts'
+import { compileMustaches } from './_mustache-compiler.ts'
 
 type Indentation = '' | ' '
 
 interface CodeFenceMatchGroups {
 	content: string
-	indentation: `${Indentation}${Indentation}${Indentation}`
-	info: string
+	indentation:
+		`${Indentation}${Indentation}${Indentation}`
+	infoString: string
 	quantity: number
 	type: '`' | '~'
 }
 
 const CODE_FENCE =
-	/^(?<indentation> {0,3})(?<quantity>(?<type>[`~])\k<type>{2,})(?<info>((?!`).)*?)\n(?<content>.*?)\n\k<quantity>\k<type>*/gms
-const CODE_FENCE_RELAX_INFOS = ['relax-module', 'relax-component']
+	/(?<=^|\n)(?<indentation> {0,3})(?<quantity>(?<type>[`~])\k<type>{2,})[ \t]*(?<infoString>(?!\k<type>)(?:.*?))[ \t]\n(?:(?<content>.*?))?(?:\n(?:\k<quantity>\k<type>*)|$)/gs
 
 export function compile(source: string, base: URL) {
 	const scripts: string[] = []
@@ -22,44 +23,28 @@ export function compile(source: string, base: URL) {
 			groups: CodeFenceMatchGroups
 		}
 
-		if (!CODE_FENCE_RELAX_INFOS.includes(groups.info.trim())) break
+		groups.infoString.replace(/^[ \t]*(.*?)[ \t]*$/, '')
 
 		source = source.replace(match[0], '')
 
 		const content = groups.content.replaceAll(
-			new RegExp(`^ {0,${groups.indentation.length}}`, 'gm'),
+			new RegExp(
+				`^ {0,${groups.indentation.length}}`,
+				'gm',
+			),
 			'',
 		)
 
-		const parsed = parse(content, {
-			syntax: 'typescript',
-			tsx: true,
-			decorators: true,
-			dynamicImport: true,
-			comments: true,
-			script: false,
-			target: 'es2022',
-		})
+		const parsed = parse(content)
 
-		scripts.push(print(parsed).code)
+		scripts.push(print(parsed))
 	}
 
 	const result = `export function component() {
     ${scripts.join('')}
+
+	return \`${compileMustaches(source)}\`
 }`
 
 	return result
-}
-
-if (import.meta.main) {
-	console.log(
-		compile(
-			`~~~ relax-component   
-let a = 0
-~~~
-
-Lorem ipsum.`,
-			new URL('https://ayoreis.com'),
-		),
-	)
 }
