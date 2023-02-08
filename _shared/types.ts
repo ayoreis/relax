834f5d94c0b6e8ebb7fcff5b3f4affe86e1aba05
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-02-08 18:18:01 +0000

    refactor: Delete stuff

--- _shared/types.ts
+++ _shared/types.ts
@@ -1,8 +1,1 @@
 export type MaybePromise<Type> = Promise<Type> | Type
-
-export type KeyOfMap<TheMap extends Map<unknown, unknown>> =
-	TheMap extends Map<infer Key, unknown> ? Key : never
-
-export type ValueOfMap<
-	TheMap extends Map<unknown, unknown>,
-> = TheMap extends Map<unknown, infer Value> ? Value : never
